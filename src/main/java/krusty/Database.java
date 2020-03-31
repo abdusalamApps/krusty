6a6a0397b0b9c085a852661cfb7fd760b45d0601
abdo <abdelsalem.yabrag@hotmail.com>
2020-03-31 11:02:34 +0200

    Initial commit

---
+++ src/main/java/krusty/Database.java
@@ -0,0 +1,98 @@
+package krusty;
+
+import spark.Request;
+import spark.Response;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+import static krusty.Jsonizer.toJson;
+
+public class Database {
+	/**
+	 * Modify it to fit your environment and then use this string when connecting to your database!
+	 */
+	private static final String jdbcString = "jdbc:mysql://35.228.254.209/krusty";
+
+	// For use with MySQL or PostgreSQL
+	private static final String jdbcUsername = "abdo";
+	private static final String jdbcPassword = "";
+
+	private static Connection connection;
+
+	public void connect() {
+		try {
+			connection = DriverManager.getConnection(jdbcString, jdbcUsername, jdbcPassword);
+
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}
+
+	// TODO: Implement and change output in all methods below!
+	public String getCustomers(Request req, Response res) {
+		String customers = "{}";
+		try {
+			PreparedStatement preparedStatement =
+					connection.prepareStatement("select * from Customers");
+			 customers = toJson(preparedStatement.executeQuery(), "customers");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return customers;
+	}
+
+	public String getRawMaterials(Request req, Response res) {
+		return "{}";
+	}
+
+	public String getCookies(Request req, Response res) {
+		return "{\"cookies\":[]}";
+	}
+
+	public String getRecipes(Request req, Response res) {
+		return "{}";
+	}
+
+	public String getPallets(Request req, Response res) {
+		return "{\"pallets\":[]}";
+	}
+
+	public String reset(Request req, Response res) {
+		try {
+			Statement statement = connection.createStatement();
+
+			InputStream inputStream = getClass().getResource("/reset.sql").openStream();
+
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+			bufferedReader.lines()
+					.filter(line -> !line.trim().startsWith("--") || !line.trim().isEmpty())
+					.forEach(line -> {
+						try {
+							statement.addBatch(line.trim());
+						} catch (SQLException e) {
+							System.out.println("Line: " + line);
+							e.printStackTrace();
+						}
+					});
+			statement.executeBatch();
+			statement.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Could not open reset file.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "{}";
+	}
+
+	public String createPallet(Request req, Response res) {
+		return "{}";
+	}
+}
---
+++ src/main/resources/reset.sql
@@ -0,0 +1,10 @@
+truncate Customers;
+
+insert into Customers (name, address) VALUES ('Bjudkakor AB', 'Ystad');
+insert into Customers (name, address) VALUES ('Finkakor AB', 'Helsingborg');
+insert into Customers (name, address) VALUES ('Gästkakor AB', 'Hässleholm');
+insert into Customers (name, address) VALUES ('Kaffebröd AB', 'Landskrona');
+insert into Customers (name, address) VALUES ('Kalaskakor AB', 'Trelleborg');
+insert into Customers (name, address) VALUES ('Partykakor AB', 'Kristianstad');
+insert into Customers (name, address) VALUES ('Skånekakor AB', 'Perstorp');
+insert into Customers (name, address) VALUES ('Småbröd AB', 'Malmö');
