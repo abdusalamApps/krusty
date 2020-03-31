93d35cf68b90be2a86f78e51a397ff767ab1494d
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2020-03-31 12:27:35 +0200

    Added missing fields to raw materials

--- src/initial-data.sql
+++ src/initial-data.sql
@@ -1,4 +1,4 @@
-INSERT into Customers(name, adress) VALUES
+INSERT into Customers(name, address) VALUES
 ('Bjudkakor AB','Ystad'),
 ('Finkakor AB','Helsingborg'),
 ('Gästkakor AB','Hässleholm'),
@@ -15,23 +15,23 @@ INSERT into Cookies(name) VALUES
 ('Tango'),
 ('Almond delight');
 
-INSERT into RawMaterials(name,amount,unit) VALUES
-('Bread crumbs','500000','g'),
-('Butter','500000','g'),
-('Chocolate','500000','g'),
-('Chopped almonds','500000','g'),
-('Cinnamon','500000','g'),
-('Egg whites','500000','ml'),
-('Eggs','500000','g'),
-('Fine-ground nuts','500000','g'),
-('Flour','500000','g'),
-('Ground roasted nuts','500000','g'),
-('Icing sugar','500000','g'),
-('Marzipan','500000','g'),
-('Potato starch','500000','g'),
-('Roasted chopped nuts','500000','g'),
-('Sodium bicarbonate','500000','g'),
-('Sugar','500000','g'),
-('Vanilla','500000','g'),
-('Vanilla sugar','500000','g'),
-('Wheat flour','500000','g');
+INSERT into RawMaterials(raw_material_name,amount,unit,last_bought_date, last_bought_amount) VALUES
+('Bread crumbs',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Butter',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Chocolate',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Chopped almonds',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Cinnamon',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Egg whites',500000,'ml', UNIX_TIMESTAMP(), 500000),
+('Eggs',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Fine-ground nuts',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Flour',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Ground roasted nuts',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Icing sugar',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Marzipan',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Potato starch',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Roasted chopped nuts',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Sodium bicarbonate',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Sugar',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Vanilla',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Vanilla sugar',500000,'g', UNIX_TIMESTAMP(), 500000),
+('Wheat flour',500000,'g', UNIX_TIMESTAMP(), 500000);
